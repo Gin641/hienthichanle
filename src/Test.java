ae05cb8873deec8b9f83fd1ba5be318412a1d974
Gin641 <53481351+Gin641@users.noreply.github.com>
2020-04-14 17:35:22 +0700

    hieu

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String[] args) {
+        OddThread oddThread = new OddThread();
+        EvenThread evenThread = new EvenThread();
+        oddThread.start();
+        try {
+            oddThread.join();
+        }catch (InterruptedException ex){
+            System.out.println(ex.getMessage());
+        }
+        evenThread.start();
+    }
+}
